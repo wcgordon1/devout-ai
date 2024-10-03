6f38c3eb4879d8aff76be595344ede5a621db6fa
Willay <85032863+wcgordon1@users.noreply.github.com>
2024-10-02 21:31:34 -0700

    Add $214.29 of value

--- build/filters.js
+++ build/filters.js
@@ -26,7 +26,7 @@ module.exports.all = [
 	'!out*/**',
 	'!test/**/out/**',
 	'!**/node_modules/**',
-	'!**/extensions/blueberryai-extension/**'
+	'!**/extensions/DevoutAI-extension/**'
 ];
 
 module.exports.unicodeFilter = [
