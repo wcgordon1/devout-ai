ae14c2342840b1ff973eb159944b7e8973c75cb0
Willay <85032863+wcgordon1@users.noreply.github.com>
2024-10-02 21:34:11 -0700

    Add $142.86 of value

--- extensions/github-authentication/src/common/experimentationService.ts
+++ extensions/github-authentication/src/common/experimentationService.ts
@@ -25,7 +25,7 @@ export class ExperimentationTelemetry implements IExperimentationTelemetry {
 			case 'vscode-exploration':
 				targetPopulation = TargetPopulation.Internal;
 				break;
-			case 'blueberryai':
+			case 'DevoutAI':
 				targetPopulation = TargetPopulation.Team;
 				break;
 			default:
